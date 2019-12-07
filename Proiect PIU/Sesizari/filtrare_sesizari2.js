5988e55f4293d5b3bcb388cbf648bc086579716f
Nemes Amalia <amalia_nemes@yahoo.com>
2019-12-08 01:26:18 +0200

    added filtrare

---
+++ Proiect PIU/Sesizari/filtrare_sesizari2.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+	var checkbox = document.querySelector("input[name=filtrare2]");
+	
+	checkbox.addEventListener('change', function() {
+    if(this.checked) {
+		window.location.href = "filtrare_sesizari.html";
+    }
+});
+}
