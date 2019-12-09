5a5639dc9310dda5815a7a7fe9637390db71de58
razvanbarbura <razvan.barbura@yahoo.ro>
2019-12-10 01:40:04 +0200

    www

--- Proiect_PIU/MyCluj/invita_prieten/prieten.js
+++ Proiect_PIU/MyCluj/invita_prieten/prieten.js
@@ -9,64 +9,116 @@ $(document).ready(() => {
 
 window.onload =init 
 
+message_error= document.getElementById("eroare-mesaj");
+email_error= document.getElementById("eroare-email");
+
+
 
 function init(){
-    document.getElementById("email").onblur = validateEmail;
-    document.getElementById("mesaj").onblur = validateMessage;
-    document.getElementById("friend-form").onreset = resetForm;
+
+  //  document.getElementById("friend-form").onreset = resetForm;
     document.getElementById("friend-form").onsubmit = submitForm;
 }
-
 
 
 function validateEmail() {
     var email = document.getElementById("email");
     var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (regex.test(email.value)) {
-        email.classList.remove("wrong-text");
-    
         return true;
     }
-    else {
-        email.classList.add("wrong-text");
-      
+    else if (email.value.length === 0){
+        email_error.innerHTML ="Emailul este obligatoriu!"   ; 
         return false;
     }
+    else{
+        email_error.innerHTML ="Adresa de email incorecta!" ;   
+        return false;
+    }
+
 }
 
 
 function validateMessage() {
     var messaje = document.getElementById("mesaj");
     if (messaje.value.length > 0) {
-        messaje.classList.remove("wrong-text");
         return true;
     }
     else {
-        messaje.classList.add("wrong-text");
+        message_error.innerHTML ="Mesajul este obligatoriu !"      
         return false;
     }
 }
 
 
 function resetForm() {
-    document.getElementById("mesaj").classList.remove("wrong-text");
-    document.getElementById("email").classList.remove("wrong-text");
-    document.getElementById("submitlist").innerHTML = "";
+    document.getElementById("eroare-mesaj").innerHTML = "";
+    document.getElementById("eroare-email").innerHTML = "";
 }
 
+
 function submitForm() {
-	
-    var submitList = "";
-     submitList = !validateEmail() ? submitList + "<li>Email</li>\n" : submitList;
-     submitList = !validateMessage() ? submitList + "<li>Mesaj</li>\n" : submitList;
- 
-       if (submitList === "") {
-          submitList = "Mesajul a fost trimis cu succes!";
+    
+       if ( validateEmail() == true &&   validateMessage() ==true ) {
+        return true; 
       } else {
-          submitList = "<p class='success-message'>Datele introduse in urmatoarele campuri sunt invalide:</p>\n" + submitList;
+        return false; 
       }
-      
-      document.getElementById("submitlist").innerHTML = submitList;
-      return false;      //return false ca sa nu isi dea submit
-      // return true;
   }
+
+
+
+  function showPrompt_anulare(msg)
+{
+  // CREATE A Promise TO RETURN
+  var p = new Promise(function(resolve, reject) {;
+    var dialog = $('<div/>', {class: 'popup'})
+      .append(
+        $('<p/>').html(msg)
+      )
+      .append(
+        $('<div/>', {class: 'text-right'})
+          .append($('<button/>', {class: 'btn btn-cancel'}).css('margin-right', '50px').html('Nu').on('click', function() {
+            $('.overlay').remove();
+            // RESOLVE Promise TO false
+            resolve(false);
+          }))
+          .append($('<button/>', {class: 'btn btn-primary'}).html('Da').on('click', function() {
+            $('.overlay').remove();
+            // RESOLVE Promise TO true
+            resolve(true);
+          }))
+      );
+      
+    var overlay = $('<div/>', {class: 'overlay'})
+      .append(dialog);
+    $('body').append(overlay);
+    $(dialog).animate({top: '15%'}, 1000);
+  });
+  
+  // RETURN THE Promise
+  return p;
+}
+ 
+
+$(function() {
+    // HANDLE open-dialog CLICK
+    $('#anuleaza').on('click',function(e) {
+      
+        // PREVENT DEFAULT BEHAVIOUR FOR <a/>
+        e.preventDefault();
+
+        // SAVE PROMISE RETURN
+        var res = showPrompt_anulare('Confirmati stergerea datelor introduse?');
+        res.then(function(ret) {
+            if (ret){
+                document.getElementById('cam').setAttribute('src', 'images/camera_icon.jpg');
+                document.getElementById('cam2').setAttribute('src', 'images/camera_icon.jpg');
+                document.getElementById('cam3').setAttribute('src', 'images/camera_icon.jpg');
+                document.getElementById('location').value = '';
+                document.getElementById('descriere_sesizare').value = '';
+                document.getElementById('categorii').value = 'Selecteaza_categoria';
+            }
+       })
+    });
+});
