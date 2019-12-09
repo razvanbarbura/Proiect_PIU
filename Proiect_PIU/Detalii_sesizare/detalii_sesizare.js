b3dd94bd26e35f4088e572cd9cb0165c732f1d1a
razvanbarbura <razvan.barbura@yahoo.ro>
2019-12-09 12:26:44 +0200

    Update 2

--- Proiect_PIU/Detalii_sesizare/detalii_sesizare.js
+++ Proiect_PIU/Detalii_sesizare/detalii_sesizare.js
@@ -6,4 +6,60 @@ $(document).ready(() => {
 
 });
 
+var numberOfLikes = 3;
+var numberOfDislikes = 0;
+var likePressed = false;
+var dislikePressed = false;
 
+function like(){
+    likePressed = !likePressed
+    var like = document.getElementById("like_number");
+    var dislike = document.getElementById("dislike_number");
+    if(likePressed){
+        if(dislikePressed){
+            dislikePressed = false;
+            numberOfDislikes-- ;
+           
+            dislike.innerHTML =""+numberOfDislikes
+            
+        }
+        numberOfLikes++;
+    }
+    else{
+        numberOfLikes--;
+    }
+    like.innerHTML =""+numberOfLikes
+
+}
+
+function dislike(){
+    dislikePressed = !dislikePressed
+    var like = document.getElementById("like_number");
+    var dislike = document.getElementById("dislike_number");
+    if(dislikePressed){
+        if(likePressed){
+            likePressed = false;
+            numberOfLikes-- ;
+    
+            like.innerHTML =""+numberOfLikes
+        }
+        numberOfDislikes++;
+      
+    }
+    else{
+        numberOfDislikes--;
+    }
+    dislike.innerHTML =""+numberOfDislikes
+
+}
+
+
+function addComent(){
+    var messages = document.getElementById("messageList");
+    var mesaj =  document.getElementById("comment");
+    if(mesaj.value.length > 0)
+    {
+        messages.innerHTML += "<li>" + "Mircea: "+ mesaj.value + "</li>";
+        mesaj.value="";
+    }
+}
