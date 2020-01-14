334ab33c3d0955e0b0a009fb82c65ec95df082c1
NemesAmalia <nemes.amalia@gmail.com>
2020-01-14 12:07:06 +0200

    adaugare sesizare

--- Proiect_PIU/MyCluj/galerie/galerie.js
+++ Proiect_PIU/MyCluj/galerie/galerie.js
@@ -1,2 +1,10 @@
 window.onload = function(){
 }
+
+function inapoi(){
+	window.location.href = "adaugare_sesizare.html";
+}
+
+function inapoi2(){
+	window.location.href = "adaugare_sesizare2.html";
+}
